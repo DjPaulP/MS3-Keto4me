55de0f14b9a582ac9ed4d3e0181843afe744e494
DjPaulP <Paul@paulprenderville.ie>
2021-07-28 14:26:47 +0000

    Add login functionality to website

--- app.py
+++ app.py
@@ -49,6 +49,33 @@ def register():
     return render_template("register.html")
 
 
+# Allow users to login
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check to see of username is in the database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure the password matches the one in the database
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome to Keto 4 Me {}".format(request.form.get("username")))
+            else:
+                # when the password doesn't match
+                flash("Your password and/or Username is incorrect")    
+                return redirect(url_for("login"))
+
+        else:
+            # If the username does not exist
+            flash("Your password and/or Username is incorrect")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
