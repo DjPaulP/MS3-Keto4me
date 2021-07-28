a876344f26e07c36726863bf829bacb3e7584ea4
DjPaulP <Paul@paulprenderville.ie>
2021-07-28 11:59:03 +0000

    Add Register functionality

--- app.py
+++ app.py
@@ -18,16 +18,34 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
-
+# Home page
 @app.route("/")
 @app.route("/get_recipes")
 def get_recipes():
     recipes = mongo.db.recipes.find()
     return render_template("recipes.html", recipes=recipes)
 
-
+# Allow users to register on the website
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # check if the username is already registered in the database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")  
+            return redirect(url_for("register"))  
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        #put the new user into a 'session' cookie on website
+        session["user"] = request.form.get("username").lower()
+        flash("Welcome, Registration Successful!")
     return render_template("register.html")
 
 
