0f21e35e276d6aeaa54ac345cfd7cf08314cbaba
DjPaulP <Paul@paulprenderville.ie>
2021-07-30 13:33:35 +0000

    Add the recipe categories to a dropdown menu

--- app.py
+++ app.py
@@ -104,7 +104,8 @@ def logout():
 @app.route("/add_recipe")
 def add_recipe():
     # Allow users to add recipes to the website
-    return render_template("add_recipe.html")
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("add_recipe.html", categories=categories)
 
 
 if __name__ == "__main__":
--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.collapsible').collapsible();
-  });
-
-  $(document).ready(function(){
     $('.slider').slider();
+    $('select').formSelect();
   });
