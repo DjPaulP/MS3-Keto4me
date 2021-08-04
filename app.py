5a2e171a0857d9efce55dc191c092c30f91773ae
DjPaulP <Paul@paulprenderville.ie>
2021-08-04 15:01:17 +0000

    Delete recipe from the database with confirm message

--- app.py
+++ app.py
@@ -146,6 +146,12 @@ def edit_recipe(recipe_id):
     return render_template("edit_recipe.html", recipe=recipe, categories=categories)
 
 
+@app.route("/delete_recipe<recipe_id>")
+def delete_recipe(recipe_id):
+    mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
+    flash ("Your recipe has successfully been deleted")
+    return redirect(url_for('get_recipes'))
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
